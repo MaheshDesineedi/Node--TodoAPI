4f58e91cf9507663fea34dbbe35f4de42939f16f
MaheshDesineedi <dmahesh1598@gmail.com>
2018-06-01 16:28:24 +0530

    Add POST to todos and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -9,16 +9,17 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
-  var todo = new Todo({
-    text: req.body.text
-  });
+app.post('/todos',(req,res) =>{
 
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  });
+	var todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save().then((doc)=>{
+		res.send(doc);
+	}, (e) =>{
+		res.send(e);
+	});
 });
 
 app.listen(3000, () => {
