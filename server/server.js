980767bb2ca9b492a9971ca947c398d665432ac6
MaheshDesineedi <dmahesh1598@gmail.com>
2018-06-02 19:28:29 +0530

    Delete todo in the route /todo/:id

--- server/server.js
+++ server/server.js
@@ -49,8 +49,23 @@ app.get('/todo/:id', (req,res) =>{
 
 		res.send({todo});
 	}).catch((e) => res.status(400).send());
+});
 
 
+app.delete('/todo/:id',(req,res) => {
+	var id = req.params.id;
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo){
+			return res.status(404).send();
+		}
+		
+		res.send({todo});
+	}).catch((e) => res.status(404).send());
 });
 
 app.listen(port, () => {
