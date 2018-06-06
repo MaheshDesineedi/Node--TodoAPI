8779f0f7fc5779d1a6ead9fa36e2801fe949e85f
MaheshDesineedi <dmahesh1598@gmail.com>
2018-06-06 23:18:07 +0530

    user.js bug fix in findByCredentials

--- server/models/user.js
+++ server/models/user.js
@@ -82,7 +82,7 @@ UserSchema.statics.findByToken = function(token) {
 };
 
 UserSchema.statics.findByCredentials = function(email , password) {
-
+	var User = this;
 		return User.findOne({email}).then((user) => {
 			if(!user) {
 				return Promise.reject();
